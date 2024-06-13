57b47807c708f35d552f43bc4d58e14e4a4df941
Zenith <zenithyap88@gmail.com>
2024-06-13 20:41:06 +0800

    Add envelope

--- src/App.js
+++ src/App.js
@@ -1,7 +1,20 @@
 import './App.css';
 import React from 'react';
+import { useState } from 'react';
+import Envelope from './images/envelope/Envelope.png';
+import EnvelopeCover from './images/envelope/EnvelopeCover.png';
+import './animations/OpenEnvelope.css';
+import './animations/SlideLetterOut.css';
 
 function App() {
+    const [openEnvelope, isOpenEnvelope] = useState(false);
+    const [slideLetterOut, isSlideLetterOut] = useState(false);
+
+    const handleLetterClick = () => {
+        isOpenEnvelope(true);
+        isSlideLetterOut(true);
+    }
+
     return (
         <div
             style={{
@@ -12,9 +25,41 @@ function App() {
                 display: "flex",
             }}
         >
-            <h1>
-                Kang Quan
-            </h1>
+            <div 
+                style={{
+                    position: "relative",
+                    backgroundColor: "rgb(169, 125, 81)"
+                }}
+            >
+                <div
+                    className={openEnvelope ? 'openEnvelope' : ''}
+                    style={{
+                        position: "absolute",
+                        zIndex: 2
+                    }}
+                >
+                    <img src={EnvelopeCover} onClick={handleLetterClick}/>
+
+                </div>
+                <div
+                    className={slideLetterOut ? 'slideLetterOut' : ''}
+                    style={{
+                        backgroundColor: "#FFE6EE",
+                        width: "100%", 
+                        height: "100%",
+                        position: "absolute",
+                    }}
+                >
+                </div>
+                <div
+                    style={{
+                        zIndex: slideLetterOut ? 4 : 1,
+                        position: "relative"
+                    }}
+                >
+                    <img src={Envelope}/>
+                </div>
+            </div>
         </div>
     );
 }
